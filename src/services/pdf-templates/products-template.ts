88a792b3ab329a00aa4ab507fda8aeaac3712e8f
Rut Maria Martinez <152558337+Ruki-04@users.noreply.github.com>
2024-12-02 06:14:33 -0500

    add translation pdf

--- src/services/pdf-templates/products-template.ts
+++ src/services/pdf-templates/products-template.ts
@@ -1,5 +1,5 @@
 /*
- * 
+ *
  *
  * MIT License
  *
@@ -15,34 +15,26 @@ import { moveDown } from "./common";
 import { generateHr } from "./hr";
 
 export default class PdfProductsTemplate {
-  private static generateTableRow(
-    doc,
-    product,
-    variant,
-    sum,
-  ) {
+  private static generateTableRow(doc, product, variant, sum) {
     const startY = doc.y;
     doc
       .fontSize(10)
       .text(product, 70, startY, { align: "left" })
       .text(variant, 200, startY, { align: "left" })
-      .text(sum, 320, startY, { width: 90, align: "right" })
-    }
+      .text(sum, 320, startY, { width: 90, align: "right" });
+  }
 
-  static generateTable(doc, variantsCountPopularityResult: VariantsCountPopularityResult) : void {
-  
+  static generateTable(
+    doc,
+    variantsCountPopularityResult: VariantsCountPopularityResult
+  ): void {
     doc.font("Helvetica-Bold");
-    this.generateTableRow(
-      doc,
-      "Product",
-      "Variant",
-      "Sum",
-    );
-    moveDown(doc)
+    this.generateTableRow(doc, "Producto", "Variante", "Suma");
+    moveDown(doc);
     generateHr(doc);
-    moveDown(doc)
+    moveDown(doc);
     doc.font("Helvetica");
-  
+
     let totalCurrent = 0;
     for (const currentResults of variantsCountPopularityResult.current) {
       totalCurrent += Number(currentResults.sum);
@@ -52,31 +44,23 @@ export default class PdfProductsTemplate {
         currentResults.variantTitle,
         currentResults.sum
       );
-  
-      moveDown(doc)
+
+      moveDown(doc);
       generateHr(doc);
-      moveDown(doc)
+      moveDown(doc);
     }
-  
-    this.generateTableRow(
-      doc,
-      "Total",
-      "",
-      totalCurrent
-    );
-    moveDown(doc)
+
+    this.generateTableRow(doc, "Total", "", totalCurrent);
+    moveDown(doc);
   }
-  
-  static generateHeader(doc) : void {
-    doc
-      .fontSize(18)
-    moveDown(doc)
-  
-    doc
-      .text('Top products', 70, doc.y, { align: "left"})
-    
-    doc
-      .fontSize(12)
-    moveDown(doc)
+
+  static generateHeader(doc): void {
+    doc.fontSize(18);
+    moveDown(doc);
+
+    doc.text("Productos destacados", 70, doc.y, { align: "left" });
+
+    doc.fontSize(12);
+    moveDown(doc);
   }
 }
--- src/services/pdf-templates/sales-template.ts
+++ src/services/pdf-templates/sales-template.ts
@@ -1,5 +1,5 @@
 /*
- * 
+ *
  *
  * MIT License
  *
@@ -17,47 +17,33 @@ import { amountToDisplay } from "../utils/currency";
 import { moveDown } from "./common";
 
 export default class PdfSalesTemplate {
-  private static generateTableRow(
-    doc,
-    date,
-    currencyCode,
-    sales
-  ) {
+  private static generateTableRow(doc, date, currencyCode, sales) {
     const startY = doc.y;
     doc
       .fontSize(10)
       .text(date, 70, startY, { align: "left" })
       .text(currencyCode, 200, startY, { align: "left" })
-      .text(sales, 320, startY, { width: 90, align: "right" })
+      .text(sales, 320, startY, { width: 90, align: "right" });
   }
 
   static generateTableTitle(doc, region: Region) {
-    doc
-      .fontSize(14)
-    moveDown(doc)
+    doc.fontSize(14);
+    moveDown(doc);
 
-    doc
-      .text(`${region.name}`, 70, doc.y, { align: "left"})
-    
-    doc
-      .fontSize(12)
-    moveDown(doc)
+    doc.text(`${region.name}`, 70, doc.y, { align: "left" });
+
+    doc.fontSize(12);
+    moveDown(doc);
   }
-  
-  static generateTable(doc, salesHistoryResult: SalesHistoryResult) : void {
-  
+
+  static generateTable(doc, salesHistoryResult: SalesHistoryResult): void {
     doc.font("Helvetica-Bold");
-    this.generateTableRow(
-      doc,
-      "Date",
-      "Currency code",
-      "Sales",
-    );
-    moveDown(doc)
+    this.generateTableRow(doc, "Fecha", "Código de moneda", "Ventas");
+    moveDown(doc);
     generateHr(doc);
-    moveDown(doc)
+    moveDown(doc);
     doc.font("Helvetica");
-  
+
     let totalCurrent = 0;
     for (const currentSalesResult of salesHistoryResult.current) {
       totalCurrent += Number(currentSalesResult.total);
@@ -65,34 +51,33 @@ export default class PdfSalesTemplate {
         doc,
         currentSalesResult.date.toLocaleDateString(),
         salesHistoryResult.currencyCode.toUpperCase(),
-        amountToDisplay(Number(currentSalesResult.total), salesHistoryResult.currencyCode)
+        amountToDisplay(
+          Number(currentSalesResult.total),
+          salesHistoryResult.currencyCode
+        )
       );
-  
-      moveDown(doc)
+
+      moveDown(doc);
       generateHr(doc);
-      moveDown(doc)
+      moveDown(doc);
     }
-  
+
     this.generateTableRow(
       doc,
       "Total",
       "",
       amountToDisplay(totalCurrent, salesHistoryResult.currencyCode)
     );
-    moveDown(doc)
+    moveDown(doc);
   }
-  
-  static generateHeader(doc) : void {
-    doc
-      .fontSize(18)
-    moveDown(doc)
-    
 
-    doc
-      .text('Sales by region', 70, doc.y, { align: "left"})
-    
-    doc
-      .fontSize(12)
-    moveDown(doc)
+  static generateHeader(doc): void {
+    doc.fontSize(18);
+    moveDown(doc);
+
+    doc.text("Ventas por región", 70, doc.y, { align: "left" });
+
+    doc.fontSize(12);
+    moveDown(doc);
   }
 }
